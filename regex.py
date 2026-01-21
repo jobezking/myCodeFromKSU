1911b970ec3d62bc097880ddf40251e26b8155e1
jobezking <52982865+jobezking@users.noreply.github.com>
2026-01-21 12:19:31 -0500

    Implement regex search examples in regex.py
    Added regex search examples demonstrating various patterns.

---
+++ regex.py
@@ -0,0 +1,27 @@
+import re
+str = 'an example word:cat!!"
+match = re.search(r'word:\w\w\w',str)
+if match:
+  print('found', match.group())
+else:
+  print('did not find)
+
+### Search for pattern 'iii' in string 'piiig'.
+### All of the pattern must match but it may appear anywhere.
+### On success, match.group() is matched text.
+match = re.search(r'iii', 'piiig)
+match.group()
+
+match = re.search(r'igs', 'piiig')
+print(match)
+
+## . = any char but \n
+match = re.search('r..g', 'piiig')
+match.group()
+
+## \d = digit char, \w = word char
+match = re.search(r'\d\d\d', 'p123g')
+match.group()
+
+match = re.search('r'\w\w\w', '@@abcd!!')
+match.group()
