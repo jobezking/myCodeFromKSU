6a815b9e9657ae3004a66e90f683acacc9d3f279
Job King <jobezking@gmail.com>
2026-03-02 17:56:10 -0500

    first

--- cs7357/hugging.py
+++ cs7357/hugging.py
@@ -1,3 +1,25 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+from tqdm import tqdm
+
+import torchaudio
+
+import wandb
+from dotenv import load_dotenv
+
+load_dotenv() # Loads the variables from .env
+wandb_key  = os.getenv("WANDB_API_KEY")
+token = os.getenv("HF_TOKEN")
+wandb.login(key=wandb_key)
+from huggingface_hub import login
+login(token=HF_TOKEN)
+
+
 from datasets import load_dataset
 
 data_files = {
@@ -16,3 +38,15 @@ valid_data = dataset['valid']
 
 print(train_data)
 print(valid_data)
+
+repo_name = "wav2vec2-large-xls-r-300m-dm32"
+
+input_col = 'path'
+output_col = 'label'
+audio_len = 32
+
+label_list = train_data.unique(output_col)
+label_list.sort()
+num_classes = len(label_list)
+print(f"Number of classes: {num_classes}")
+print(f"Classes: {label_list}")
